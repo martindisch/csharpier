ff3ec48e1967045aa1a4546ef449d36c717330eb
Martin Disch <Martin.Disch@digitecgalaxus.ch>
2024-12-12 10:56:44 +0100

    Add tool to update snapshots with `dotnet run --project AcceptTests`

---
+++ AcceptTests/Program.cs
@@ -0,0 +1,8 @@
+﻿var actualFiles = Directory.GetFiles("Src/CSharpier.Tests", "*.actual.test", SearchOption.AllDirectories);
+
+foreach (var file in actualFiles)
+{
+    var expectedFileExists = File.Exists(file.Replace(".actual.test", ".expected.test"));
+    var targetExtension = expectedFileExists ? ".expected.test" : ".test";
+    File.Move(file, file.Replace(".actual.test", targetExtension), overwrite: true);
+}
